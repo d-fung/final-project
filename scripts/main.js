0bcc861ffb7a6daeb5808de02bc4546cffad92e6
donna fung <donnaskfung@gmail.com>
2015-06-24 20:14:38 -0400

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+alert('Hello World!');
+
+var myHeading = document.querySelector('h1'); 
+myHeading.innerHTML = 'Hello world!';
+
+var myHeading = document.getElementById('testing');
+myHeading.innerHTML = 'this is cool!';
+
+alert(32 + 9 *1.8);
+
+var num_total = 9+5;
+var string_total = "this is your age:" + num_total + "and today's date is:" + Date();
+
+document.getElementById('pagetitle').innerHTML = string_total;
