d06cc60bade3a61fabd4f066ed279065505a644a
donna fung <donnaskfung@gmail.com>
2015-07-08 21:26:56 -0400

    after july 8 class

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -8,12 +8,36 @@ $(window).scroll(function() {
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
+
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').on('click', function(event) {
+        var fromTop = 106;
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: $($anchor.attr('href')).offset().top - fromTop 
+        }, 900, 'easeInOutExpo');
         event.preventDefault();
     });
 });
+
+// $(document).ready(function() {
+
+//     var offsetHeight = 51;
+
+//     $('body').scrollspy({
+//         offset: offsetHeight
+//     });
+
+//     $('.navbar li a').click(function (event) {
+//         var scrollPos = $('body > .container').find($(this).attr('href')).offset().top - (offsetHeight - 1);
+//         $('body,html').animate({
+//             scrollTop: scrollPos
+//         }, 500, function () {
+//             $(".btn-navbar").click();
+//         });
+//         return false;
+//     });
+
+// });
+
+
